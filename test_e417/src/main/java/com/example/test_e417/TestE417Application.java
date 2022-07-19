ac0be42e7b295af60342409b6129c6a78966472c
Усубалиев Эльзамин Элишадович <e.usubaliev@echelon.lan>
2022-07-19 18:26:56 +0300

    first commit2

---
+++ test_e417/src/main/java/com/example/test_e417/TestE417Application.java
@@ -0,0 +1,13 @@
+package com.example.test_e417;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestE417Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestE417Application.class, args);
+    }
+
+}
