94c75ad10cd5c67b6ed57b25112fecfcba4a0ec4
Усубалиев Эльзамин Элишадович <e.usubaliev@echelon.lan>
2022-08-01 12:43:21 +0300

    front

---
+++ test_e417/src/main/java/com/example/test_e417/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.example.test_e417.repositories;
+
+import com.example.test_e417.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAll();
+
+    Project findByProjectIdentifier(String projectId);
+}
