f5341362fb38130d3a65e35159993bac7c05f40c
Усубалиев Эльзамин Элишадович <e.usubaliev@echelon.lan>
2022-08-01 16:25:47 +0300

    fewfwe

--- test_react_417/src/reducers/index.js
+++ test_react_417/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
+import projectReduser from "./projectReduser";
 
 export default combineReducers({
   errors: errorReducer,
+  project: projectReduser,
 });
