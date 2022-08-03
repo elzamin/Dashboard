7bbcbb25026b79e59e7e0ff1fa92ec23fe917c4c
Усубалиев Эльзамин Элишадович <e.usubaliev@echelon.lan>
2022-08-03 14:27:16 +0300

    2

--- test_e417/src/main/java/com/example/test_e417/services/ProjectService.java
+++ test_e417/src/main/java/com/example/test_e417/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.example.test_e417.services;
 
+import com.example.test_e417.domain.Backlog;
 import com.example.test_e417.domain.Project;
 import com.example.test_e417.exceptions.ProjectIdException;
+import com.example.test_e417.repositories.BacklogRepository;
 import com.example.test_e417.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,27 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOUpdateProject(Project project){
 
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId()==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId()!=null){
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+
             return projectRepository.save(project);
+
         } catch (Exception e) {
             throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + "already exists");
         }
