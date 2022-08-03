246106ebe35b769f77c07212102dadb4f3227bd6
Усубалиев Эльзамин Элишадович <e.usubaliev@echelon.lan>
2022-08-03 17:05:35 +0300

    5

--- test_e417/src/main/java/com/example/test_e417/services/ProjectTaskService.java
+++ test_e417/src/main/java/com/example/test_e417/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.example.test_e417.services;
 
+import com.example.test_e417.domain.Backlog;
 import com.example.test_e417.domain.ProjectTask;
 import com.example.test_e417.repositories.BacklogRepository;
 import com.example.test_e417.repositories.ProjectRepository;
@@ -16,15 +17,32 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        //Exception: Project not found
 
         //PTs to be added to a specific project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
         //set the bl to pt
+        projectTask.setBacklog(backlog);
         //we want our project ... IDPRO-1 IDPRO-2 ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
         //Update the BL SEQUENCE
+        BacklogSequence++;
+
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
         //INITIAL priority when priority is null
+        //if(projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+        //    projectTask.setPriority(3);
+        //}
         //INITIAL status when status is null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+        }
 
+        return projectTaskRepository.save(projectTask);
     }
 }
