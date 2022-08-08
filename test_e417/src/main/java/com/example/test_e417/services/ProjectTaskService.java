48d4f1a03e933da8503cd1aacf544662c6b0cfae
Усубалиев Эльзамин Элишадович <e.usubaliev@echelon.lan>
2022-08-08 17:05:15 +0300

    13

--- test_e417/src/main/java/com/example/test_e417/services/ProjectTaskService.java
+++ test_e417/src/main/java/com/example/test_e417/services/ProjectTaskService.java
@@ -24,7 +24,8 @@ public class ProjectTaskService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+    public ProjectTask addProjectTask(String projectIdentifier,
+                                      ProjectTask projectTask) {
 
         try {
             //PTs to be added to a specific project, project != null, BL exists
@@ -68,11 +69,12 @@ public class ProjectTaskService {
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 
-    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
+    public ProjectTask findPTByProjectSequence(String backlog_id,
+                                               String pt_id) {
 
         Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
         if (backlog == null) {
-            throw new ProjectNotFoundException("Project with ID: '" + backlog_id + "' not found");
+            throw new ProjectNotFoundException("Project with ID: '" + backlog_id + "' does not exist");
         }
 
         ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
@@ -87,11 +89,28 @@ public class ProjectTaskService {
         return projectTask;
     }
 
-    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask,
+                                               String backlog_id,
+                                               String pt_id) {
+
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
         projectTask = updatedTask;
 
         return projectTaskRepository.save(projectTask);
+    }
+
+    public void deletePMByProjectSequence(String backlog_id,
+                                          String pt_id) {
+
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+
+        projectTaskRepository.delete(projectTask);
     }
 
 }
--- test_e417/src/main/java/com/example/test_e417/web/BacklogController.java
+++ test_e417/src/main/java/com/example/test_e417/web/BacklogController.java
@@ -64,6 +64,15 @@ public class BacklogController {
         ProjectTask updatedProjectTask = projectTaskService.updateByProjectSequence(projectTask, backlog_id, pt_id);
 
         return new ResponseEntity<ProjectTask>(updatedProjectTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id,
+                                               @PathVariable String pt_id){
+
+        projectTaskService.deletePMByProjectSequence(backlog_id, pt_id);
+
+        return new ResponseEntity<String>("Project Task '"+pt_id+"' was deleted successfully", HttpStatus.OK);
 
     }
 }
