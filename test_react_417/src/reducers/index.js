c0f1429ceac76144fe398ccda466436004a112de
elzamin <elzamin006@mail.ru>
2023-01-17 13:21:51 +0300

    f4

--- test_react_417/src/reducers/index.js
+++ test_react_417/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReduser from "./projectReduser";
 import backlogReducer from "./backlogReducer";
+import securityReduser from "./securityReduser";
 
 export default combineReducers({
   errors: errorReducer,
   project: projectReduser,
   backlog: backlogReducer,
+  security: securityReduser,
 });
